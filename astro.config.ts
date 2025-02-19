6aad7c7a084ba1343305536d6d740ce98abeafb1
5ma <51451702+5ma@users.noreply.github.com>
2025-02-19 19:46:14 +0900

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,28 @@
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://playground.shiftbrain.com/',
+  base: '/post/template',
+  server: {
+    open: '/post/template',
+  },
+  prefetch: true,
+  integrations: [
+    mdx(),
+    tailwind({
+      nesting: true,
+    }),
+    icon(),
+    react(),
+  ],
+  vite: {
+    define: {
+      'import.meta.vitest': 'undefined',
+    },
+  },
+})
